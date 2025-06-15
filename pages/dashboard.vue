e98f9e2a051a620f7f5d87007881bbd896211b5e
Jesse Beck <thewebbeckons@users.noreply.github.com>
2025-06-15 13:44:03 -0400

    Updated dashboard.vue

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -73,10 +73,7 @@
             <GuestTable />
           </Transition>
         </div>
-        <div class="space-y-4 flex flex-row justify-between items-center text-gray-400">
-          <p>© 2024 WedChart. Made with ❤️ for couples planning their perfect day.</p>
-          <NuxtLink to="coff.ee/667z79qdqdp" target="_blank">Buy me a Coffee</NuxtLink>
-        </div>
+        
       </main>
 
       <!-- Share List Modal -->
@@ -85,6 +82,10 @@
       <!-- Notifications -->
       <UNotifications />
     </div>
+    <div class="space-y-4 flex flex-row justify-between items-center text-gray-400">
+          <p>© 2024 WedChart. Made with ❤️ for couples planning their perfect day.</p>
+          <NuxtLink to="coff.ee/667z79qdqdp" target="_blank">Buy me a Coffee</NuxtLink>
+        </div>
   </AuthCheck>
 </template>
 
