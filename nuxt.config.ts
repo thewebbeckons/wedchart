26fcbe56f9754176b7976bced6a94af13d5858c8
Jesse Beck <thewebbeckons@users.noreply.github.com>
2025-06-18 19:52:02 -0400

    Update Favicon to Simple Heart Icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,10 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
+      ],
       script: [
         {
           async: true,
